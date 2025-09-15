63bbc0f8439cbde0faedba5ef476f165ec2fd5c8
Hub-Next <122001110+Satnam-Gill@users.noreply.github.com>
2025-09-15 17:12:41 +0530

    Remove: Neighbourhood

--- app/(State)/[State]/page.tsx
+++ app/(State)/[State]/page.tsx
@@ -593,7 +593,7 @@ export default async function SubdomainPage({ params }: SubdomainPageProps) {
                 {ContentData?.neighbourhoods?.split("|").map((item: any) => (
                   <div className="" key={item}>
                     <Link
-                      href={`/neighborhoods/${
+                      href={`/${
                         item
                           .trim()
                           .toLowerCase()
